cd5eb591b534f78eca9404be6906d5822558b2df
Miguel Rentes <miguel.rentes@gmail.com>
2015-02-08 15:49:04 +0000

    Initial Commit

---
+++ Challenge1/Challenge1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Challenge1
+//
+//  Created by Miguel Rentes on 08/02/15.
+//  Copyright (c) 2015 Miguel Rentes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
