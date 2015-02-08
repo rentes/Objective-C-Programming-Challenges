fe7002fd47cbb2baa9967dd1f2f80e7ad966254b
Miguel Rentes <miguel.rentes@gmail.com>
2015-02-08 16:07:30 +0000

    Challenge 1 - Two Floats

---
+++ TwoFloats/TwoFloats/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TwoFloats
+//
+//  Created by Miguel Rentes on 08/02/15.
+//  Copyright (c) 2015 Miguel Rentes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
